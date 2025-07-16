fb83d29b85c13eefb302d7c31dcd2e7a1961ac2f
PrakVisal <visalprak2015@gmail.com>
2025-07-16 22:07:08 +0700

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,37 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Oswald } from 'next/font/google'
+import { icons } from 'lucide-react';
 
 const oswald = Oswald({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
-export const metadata: Metadata = {
-  icons: 'cartoonpf.PNG',
-  title: 'PrakVisal',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+// app/page.tsx or app/portfolio/page.tsx
+export const metadata = {
+  icons: "cartoonpf-modified.png",
+  title: "Prak Visal | UI/UX Designer & Backend Developer",
+  description: "Portfolio of Prak Visal, UI/UX Designer and Backend Developer based in Cambodia.",
+  keywords: "UI/UX, Designer, Developer, Portfolio, Cambodia, Prak Visal, Backend Developer, prakvisal, visal, visalprak",
+  openGraph: {
+    title: "Prak Visal | UI/UX Designer & Backend Developer",
+    description: "Portfolio of Prak Visal, UI/UX Designer and Backend Developer based in Cambodia.",
+    url: "https://prakvisal.vercel.app",
+    images: [
+      {
+        url: "https://prakvisal.vercel.app/pf.jpg",
+        width: 800,
+        height: 600,
+        alt: "Prak Visal Portfolio",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Prak Visal | UI/UX Designer & Developer",
+    description: "Portfolio of Prak Visal, UI/UX Designer and Backend Developer based in Cambodia.",
+    images: ["https://prakvisal.vercel.app/pf.jpg"],
+  },
+};
 
 export default function RootLayout({
   children,
