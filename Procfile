77758fed5d9b837dfbe26816927ca2be7881edfb
PrakVisal <visalprak2015@gmail.com>
2026-01-01 18:48:00 +0700

    remove chat feature

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: node socket-server.js
-
--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -97,7 +97,6 @@ export default function Portfolio() {
         onSubmit={handleSubmit}
       />
       <Footer />
-      <ChatWidget />
     </div>
   )
 }
